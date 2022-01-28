75985240dae26ce75f444a379c500c37e2731a07
Anouk <anouk138@gmail.com>
2022-01-28 15:34:16 -0600

    components

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import mujer from "../Assets/img/mujer.png";
 import men from "../Assets/img/men.png";
 
 const Section = () => {
+
+    const [item, setItem] = useState(0);
+
+    const suma = (numero) =>{
+        setItem(item + numero);
+    }
+
     return (
         <main className="section-main">
-            <div class="card" style={{width: "20rem"}}>
-                <img src={mujer} class="card-img-top" alt="mujer"/>
-                    <div class="card-body">
-                        <p class="card-text">MUJER</p>
+            <div className="card" style={{width: "20rem"}}>
+                <img src={mujer} className="card-img-top" alt="mujer"/>
+                    <div className="card-body">
+                        <p className="card-text">MUJER</p>
+                        <p>Cantidad: {item}</p>
+                        <button type="button" onClick={()=> suma(1)} className="btn btn-primary m-1">+</button>
+                        <button type="button" onClick={()=> suma(-1)} className="btn btn-primary m-1">-</button>
                     </div>
             </div>
-            <div class="card" style={{width: "20rem"}}>
-                <img src={men} class="card-img-top" alt="hombre"/>
-                    <div class="card-body">
-                        <p class="card-text">HOMBRE</p>
+            <div className="card" style={{width: "20rem"}}>
+                <img src={men} className="card-img-top" alt="hombre"/>
+                    <div className="card-body">
+                        <p className="card-text">HOMBRE</p>
+                        <p>Cantidad: {item}</p>
+                        <button type="button" onClick={()=> suma(1)} className="btn btn-primary m-1">+</button>
+                        <button type="button" onClick={()=> suma(-1)} className="btn btn-primary m-1">-</button>
                     </div>
             </div>
         </main>
