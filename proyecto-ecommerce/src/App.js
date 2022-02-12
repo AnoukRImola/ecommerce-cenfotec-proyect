0c9501d66d512906bc5851f6cf6624d1cac26e2e
Anouk <anouk138@gmail.com>
2022-02-11 23:02:19 -0600

    update

--- proyecto-ecommerce/src/App.js
+++ proyecto-ecommerce/src/App.js
@@ -2,6 +2,7 @@ import Nav from "./Components/Nav";
 import Section from "./Components/Section";
 import Footer from "./Components/Footer";
 import Descuento from "./Components/Descuento";
+import EnvioCondicional from "./Components/EnvioCondicional";
 
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
       <Nav />
       <Descuento/>
       <Section />
+      <hr/>
+      <EnvioCondicional/>
       <Footer />
     </div>
   );
