018201075bef69b3bb8c57774ec864061b332c69
Anouk <anouk138@gmail.com>
2022-02-22 22:37:59 -0600

    update

--- proyecto-ecommerce/src/App.js
+++ proyecto-ecommerce/src/App.js
@@ -3,6 +3,7 @@ import Section from "./Components/Section";
 import Footer from "./Components/Footer";
 import Descuento from "./Components/Descuento";
 import EnvioCondicional from "./Components/EnvioCondicional";
+import Formulario from "./Components/Formulario";
 
 
 
@@ -14,6 +15,8 @@ function App() {
       <Section />
       <hr/>
       <EnvioCondicional/>
+      <hr/>
+      <Formulario />
       <Footer />
     </div>
   );
--- proyecto-ecommerce/src/Components/Formulario.js
+++ proyecto-ecommerce/src/Components/Formulario.js
@@ -1,13 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
+import { Formik } from "formik";
 
-class Fornulario extends Component{
-    render(){
+const Formulario = () => {
+    
         return(
-            <div>
+            <>
+             <Formik
+                 initialValues={{
+                     nombre: "",
+                     correo: ""
+                 }}            
+                 validate={(valores) => {
+                     let errores = {};
 
-            </div>
+                      //Validación nombre
+                      if(!valores.nombre){
+                          errores.nombre = "Por favor ingresa un nombre"  
+                      } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)){
+                         errores.nombre = "El nombre solo puede contener letras y espacios"
+                      }
+
+                      //Validación correo
+                      if(!valores.correo){
+                        errores.correo = "Por favor ingrese un correo válido"  
+                    } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)){
+                       errores.correo = "Por favor digite un correo válido"
+                    }
+
+                      return errores;
+                 } }
+                 onSubmit={(valores, {resetForm}) => {
+                     resetForm()
+                 }}
+             >
+                 {({values, errors, touched, handleSubmit, handleChange, handleBlur}) => (
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="nombre">Nombre</label>
+                            <input 
+                                type="text" 
+                                id="nombre" 
+                                name="nombre"
+                                value={values.nombre}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            {touched.nombre && errors.nombre && <div>{errors.nombre}</div>}
+                        </div>
+                        <div>
+                            <label htmlFor="correo">Correo</label>
+                            <input 
+                                type="email" 
+                                id="correo" 
+                                name="correo"
+                                value={values.correo}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            {touched.correo && errors.correo && <div>{errors.correo}</div>}
+                        </div>
+                        <button type="submit">Enviar</button>
+                    </form>
+                )}
+              </Formik>
+            </>
         )
-    }
+    
 }
 
 export default Formulario;
